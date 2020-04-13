187c399541c1824cf0ab2b96e240b3e468e93fd8
tanukichi41 <takaki_m@outlook.jp>
2020-04-13 21:09:32 +0900

    だいぶましになった

--- programA/ball.ino
+++ programA/ball.ino
@@ -2,6 +2,8 @@ void _ball::read(int* b) {
   for (int i = 0; i <= 15; i++) {
     // *(b + i) += (1 - LPF) * (analogRead(BALL[i]) - *(b + i));
     *(b + i) = analogRead(BALL[i]);
+
+    // *b *= 1.07;
     // if (*(b + i) <= 100) {
     //   *(b + i) = 1000;
     // }
@@ -36,10 +38,10 @@ void _ball::calc(void) {
     offset = constrain(offset, -95, 95);
 
     const int error = 1;
-    if (top > error && top < 16 - error) {
+
+    if (top != 0) {
       if (top <= error || top >= 16 - error)
         offset *= 0.4;
-        
       if (deg >= 180) {
         deg -= offset;
       } else {
@@ -51,14 +53,25 @@ void _ball::calc(void) {
     if (!digitalRead(BALL_HOLD)) {
       holdTimer = device.getTime();
       kicker.val = false;
-      if (val[top] <= 300 && top > 1 && top < 15) {
+      if (val[top] <= 300 && top > 4 && top < 12) {
         speed = 80;
+      } else if (val[top] <= 300 && top > 1 && top < 15) {
+        speed = 90;
       } else {
         speed = 100;
       }
     }
 
-    if (digitalRead(BALL_HOLD) && !(top > 1 && top < 15)) {
+    if (val[0] <= 300 && deg == 0) {
+      if(val[15] > val[1]){
+        deg = 22;
+      } else {
+        deg = 338;
+      }
+    }
+    
+
+        if (digitalRead(BALL_HOLD) && !(top > 1 && top < 15)) {
       if (device.getTime() - holdTimer >= 100) {
         kicker.val = true;
       }
@@ -78,9 +91,13 @@ void _ball::calc(void) {
 
 void _ball::readDistance(void) {
   static float tempDist;
-  tempDist +=
-      (1 - 0.1) * ((val[(top + 3) % 16] + val[(top + 13) % 16]) / 2 - tempDist);
-  Serial.println(constrain(map(tempDist, 410, 500, 5, 0), 0, 10));
+  if (false) {  // trueでローパス
+    tempDist += (1 - 0.15) *
+                ((val[(top + 3) % 16] + val[(top + 13) % 16]) / 2 - tempDist);
+  } else {
+    tempDist = (val[(top + 3) % 16] + val[(top + 13) % 16]) / 2;
+  }
+  Serial.println(constrain(map(tempDist, 420, 520, 5, 0), 0, 10));
 
   dist = constrain(map(tempDist, 400, 500, 5, 0), 1, 6);
 }
